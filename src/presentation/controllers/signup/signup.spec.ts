74e1bf6bf80ccd819f547fa349bf47c5cb325fe3
Samuel <samuelsantxxi@gmail.com>
2021-10-29 21:16:04 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -197,3 +197,23 @@ test('Should return 500 if AddAccount trows', () => {
   expect(httpResponse.statusCode).toBe(500)
   expect(httpResponse.body).toEqual(new ServerError())
 })
+
+test('Should return 200 if valid data is provided', () => {
+  const { sut } = makeSut()
+  const httpRequest = {
+    body: {
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password',
+      passwordConfirmation: 'valid_password'
+    }
+  }
+  const httpResponse = sut.handle(httpRequest)
+  expect(httpResponse.statusCode).toBe(200)
+  expect(httpResponse.body).toEqual({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@mail.com',
+    password: 'valid_password'
+  })
+})
