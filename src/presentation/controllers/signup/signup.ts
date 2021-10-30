ed5cbb4511641c4a65d37886c0b1635cd7c300b0
Samuel <samuelsantxxi@gmail.com>
2021-10-29 21:00:34 -0300

    refactor: move signup protocols to a sigle file

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,7 +1,6 @@
-import { MissingParamError, InvalidParamError } from '../erros'
-import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
-import { badRequest, serverError } from '../helper/http-helper'
-import { AddAccount } from '../../domain /usecases/add-account'
+import { MissingParamError, InvalidParamError } from '../../erros'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { badRequest, serverError } from '../../helper/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
