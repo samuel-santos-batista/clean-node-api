8408ac4f40b5be358e5453cee89f7a4709450af7
Samuel <samuelsantxxi@gmail.com>
2021-11-04 17:33:20 -0300

    feat: ensure mongodb reconnect if client is down

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,25 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { Controller, HttpResponse, HttpRequest } from '../../presentation/protocols'
+class LogControllerDecorator implements Controller {
+  controller: Controller
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
 
-export const makeSignUpController = () => {
+  handle (httpRequest:HttpRequest): Promise<HttpResponse> {
+    const httpResponse = this.controller.handle(httpRequest)
+    return httpResponse
+  }
+}
+
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }
