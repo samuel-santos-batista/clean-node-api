238669d5412f85d0ed631fe2c86525b2a565d8bc
Samuel <samuelsantxxi@gmail.com>
2021-11-13 21:24:27 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
-import { Hasher } from '../../data/protocols/cryptography/hasher'
 import bcrypt from 'bcrypt'
-import { HashComparer } from '../../data/protocols/cryptography/hash-comparer'
+import { HashComparer } from '../../../data/protocols/cryptography/hash-comparer'
+import { Hasher } from '../../../data/protocols/cryptography/hasher'
 
 export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number
--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,11 +1,11 @@
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { makeSignUpValidation } from './signup-validation'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 
 export const makeSignUpController = (): Controller => {
   const salt = 12
