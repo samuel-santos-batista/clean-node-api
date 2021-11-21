0192a12fb166a3be8a65d9bf0627aa774a5489db
Samuel <samuelsantxxi@gmail.com>
2021-11-20 23:31:03 -0300

    test: ensure DbAddSurvey throws if AddSurveyRepository throws

--- src/data/usecases/add-account/db-add-accont.spec.ts
+++ src/data/usecases/add-account/db-add-accont.spec.ts
@@ -69,7 +69,7 @@ describe('DbAddAccount UseCase', () => {
     expect(hasherSpy).toHaveBeenCalledWith('valid_password')
   })
 
-  test('Should throw if Hasher with correct throws', async () => {
+  test('Should throw if Hasher throws', async () => {
     const { sut, hasherStub } = makeSut()
     jest.spyOn(hasherStub, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.add(makeFakeAccountData())
